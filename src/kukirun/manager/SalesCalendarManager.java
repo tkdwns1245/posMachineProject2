4dbb12700191fa27b6068b822440a6774d0b51c6
DESKTOP-7OPB0LV\toosk <windxtoto123@gmail.com>
2021-02-13 16:41:28 +0900

    no message

--- src/kukirun/manager/SalesCalendarManager.java
+++ src/kukirun/manager/SalesCalendarManager.java
@@ -1,33 +1,29 @@
 package kukirun.manager;
 
-import kukirun.gui.SalesCalendar;
-
-import kukirun.gui.Sales_Status;
-import kukirun.service.SalesCalendarService;
-import daoImpl.ReceiptDaoimpl;
+import kukirun.serviceImpl.SalesCalendarServiceImpl;
 
 public class SalesCalendarManager {
 
-	SalesCalendarService salecalendarservice=new SalesCalendarService();
+	SalesCalendarServiceImpl salecalendarserviceimpl=new SalesCalendarServiceImpl();
 	
 	public int[] getMonthTotalSales(int year) {
-		return salecalendarservice.getEachMonthTotalSales(year);
+		return salecalendarserviceimpl.getEachMonthTotalSales(year);
 	}
 	
 	public int[] getDayTotalSales(int year,int month,int lastday) {
-		return salecalendarservice.getEachDayTotalSales(year,month,lastday);
+		return salecalendarserviceimpl.getEachDayTotalSales(year,month,lastday);
 	}
 	
 	public Object[][] getDayDetails(int year,int month, int day){
-		return salecalendarservice.getDayDetails(year, month, day);
+		return salecalendarserviceimpl.getDayDetails(year, month, day);
 	}
 	
 	public Object[][] getYearDetails(int year){
-		return salecalendarservice.getYearDetails(year);
+		return salecalendarserviceimpl.getYearDetails(year);
 	}
 	
 	public Object[][] getMonthDetails(int year,int month){
-		return salecalendarservice.getMonthDetails(year,month);
+		return salecalendarserviceimpl.getMonthDetails(year,month);
 	}
 	
 	
