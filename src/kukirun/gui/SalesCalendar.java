b693d6ea0487b111a2be89492154ac32a97f3041
DESKTOP-7OPB0LV\toosk <windxtoto123@gmail.com>
2021-02-14 19:06:51 +0900

    no message

---
+++ src/kukirun/gui/SalesCalendar.java
@@ -0,0 +1,349 @@
+package kukirun.gui;
+
+import java.awt.Color;
+
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+
+import daoImpl.ReceiptDaoimpl;
+import kukirun.manager.SalesCalendarManager;
+
+public class SalesCalendar {
+	
+	int createMonth=0;
+	int[] yearMonthDay= {0,0,0};
+	SalesCalendarManager calendarmanager=new SalesCalendarManager();
+	
+	public void CreateSaleCalender_Months(Sales_Status sales_status) { // 해당 년도의 1~12월 판넬 생성
+		
+		sales_status.jlistList.clear();
+		sales_status.saleCalendarPanel.removeAll();
+		sales_status.saleCalendarPanel.setLayout(new GridLayout(2,6,5,5));
+		
+		yearMonthDay[0]=(int) sales_status.yearList.getSelectedItem();
+		sales_status.contentNowLabel.setText(yearMonthDay[0]+"년");
+		
+		
+		if(sales_status.yearList.getSelectedItem()==null) return;
+
+		sales_status.jlistList = new ArrayList<JList>();
+		DecimalFormat addcomma=new DecimalFormat("###,###");
+		
+		int[] TotalArray=calendarmanager.getMonthTotalSales((int)sales_status.yearList.getSelectedItem());
+		for(createMonth=0;createMonth<12;createMonth++) {
+			
+			
+			DefaultListModel<String> lmodel=new DefaultListModel<String>();
+			lmodel.addElement((createMonth+1)+"월");
+			lmodel.addElement("　");
+			lmodel.addElement("　");
+			lmodel.addElement("　");
+			lmodel.addElement("매출 : "+addcomma.format(TotalArray[createMonth]));
+			
+			JList<String> monthJlist=new JList<String>(lmodel);
+			
+			DefaultListCellRenderer renderer = (DefaultListCellRenderer) monthJlist.getCellRenderer();
+			renderer.setHorizontalAlignment(SwingConstants.CENTER); 
+			renderer.setVerticalAlignment(SwingConstants.CENTER);
+			monthJlist.setBorder(BorderFactory.createEmptyBorder(35, 0, 0, 0));
+			monthJlist.setFont(new Font("맑은 고딕",Font.BOLD, 20));
+		
+			sales_status.jlistList.add(monthJlist);			
+			monthJlist.setBackground(new Color(0xC6D9F1));
+			
+			String name=String.valueOf(createMonth+1);
+			monthJlist.setName(name);
+			
+			monthJlist.addMouseListener(new MouseAdapter() {
+
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					// TODO Auto-generated method stub
+					sales_status.contentNowLabel.setText(yearMonthDay[0]+"년"+name+"월");
+					sales_status.monthList.setSelectedIndex(Integer.valueOf(name));
+					CreateSaleCalender_Days(sales_status.thisinstance,(int)sales_status.monthList.getSelectedItem());
+					CreateMonthDetail(sales_status.thisinstance);
+				}
+				
+			});
+			
+			
+			sales_status.saleCalendarPanel.add(monthJlist);			
+			sales_status.saleCalendarPanel.setVisible(false);
+			
+		}
+		createMonth=0;
+		System.out.println("조회 완료");
+	}
+	
+	
+	
+	
+	
+	
+	
+	int createDay=0;
+	public void CreateSaleCalender_Days(Sales_Status sales_status,int month) { // 해당 월의 1~31일 달력 만듦
+		
+		sales_status.jlistList.clear();
+		sales_status.saleCalendarPanel.removeAll();
+		sales_status.saleCalendarPanel.setLayout(new GridLayout(0,7,5,5));
+		yearMonthDay[0]=(int)sales_status.yearList.getSelectedItem();
+		yearMonthDay[1]=month;
+		
+		if(sales_status.monthList.getSelectedItem()!=null) sales_status.contentNowLabel.setText(yearMonthDay[0]+"년"+yearMonthDay[1]+"월");
+		
+		if(sales_status.yearList.getSelectedItem()==null) return;
+		
+		Calendar carlendar=Calendar.getInstance();
+		carlendar.set(yearMonthDay[0], yearMonthDay[1]-1,1);
+		
+		int startday=carlendar.get(Calendar.DAY_OF_WEEK);
+		int lastday=carlendar.getActualMaximum(Calendar.DATE);
+		
+		String day = null;
+		for(int i=0; i<7;i++) {
+			switch(i) {
+			
+				case 0:day="일"; break;
+				case 1:day="월"; break;
+				case 2:day="화"; break;
+				case 3:day="수"; break;
+				case 4:day="목"; break;
+				case 5:day="금"; break;
+				case 6:day="토"; break;
+								
+			}
+			JLabel tempLabel=new JLabel(day);
+			tempLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			tempLabel.setForeground(Color.white);
+			tempLabel.setFont(new Font("맑은 고딕",Font.BOLD, 20));
+			
+			
+			
+			JPanel tempPanel=new JPanel(new GridLayout());
+			tempPanel.setBackground(new Color(0x4F81BD));
+			tempPanel.add(tempLabel);
+			sales_status.saleCalendarPanel.add(tempPanel);
+			
+
+		}
+
+	
+		for(int i=0; i<startday-1;i++) {
+
+			sales_status.saleCalendarPanel.add(new JPanel());
+
+		}
+		
+		
+		sales_status.jlistList = new ArrayList<JList>();
+		DecimalFormat addcomma=new DecimalFormat("###,###");
+		int[] TotalArray=calendarmanager.getDayTotalSales((int)sales_status.yearList.getSelectedItem(),month,lastday);
+
+		
+		for(int createDay=0;createDay<lastday;createDay++) {
+			
+			
+			
+			DefaultListModel<String> lmodel=new DefaultListModel<String>();
+			lmodel.addElement((createDay+1)+"일");
+			lmodel.addElement("　");
+			lmodel.addElement("매출 : "+addcomma.format(TotalArray[createDay]));
+			
+			
+			JList<String> dayJlist=new JList<String>(lmodel);
+			
+			DefaultListCellRenderer renderer = (DefaultListCellRenderer) dayJlist.getCellRenderer();
+			renderer.setHorizontalAlignment(SwingConstants.CENTER); 
+			renderer.setVerticalAlignment(SwingConstants.CENTER);
+			dayJlist.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+			dayJlist.setFont(new Font("맑은 고딕",Font.BOLD, 12));
+			
+			dayJlist.setName(String.valueOf(createDay+1));
+		
+			sales_status.jlistList.add(dayJlist);			
+			sales_status.jlistList.get(createDay).setBackground(new Color(0xC6D9F1));
+
+		
+			dayJlist.addMouseListener(new MouseAdapter() {
+
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					// TODO Auto-generated method stub
+					CreateDayDetail(sales_status,Integer.valueOf(dayJlist.getName()));
+				}
+				
+			});
+			
+			
+			sales_status.saleCalendarPanel.add(dayJlist);			
+			sales_status.saleCalendarPanel.setVisible(false);
+			
+		}
+		createDay=0;
+		System.out.println("조회 완료");
+	}
+	
+
+	public void CreateDayDetail(Sales_Status sales_status, int day) { //달력에서 Day 판넬을 클릭 했을 때 Day의 상세 페이지 생성
+		
+		sales_status.jlistList.clear();
+		sales_status.saleCalendarPanel.removeAll();
+		sales_status.saleCalendarPanel.setLayout(new GridLayout(1,1,10,10));
+		
+		yearMonthDay[2]=day;
+		
+		JTable day_Sales_Table=new JTable();
+		
+		Object[] header = {"영수증 번호","메뉴 카테고리","메뉴","금액","개수","메뉴별 합 금액","총 금액"};
+	    Object[][] data = calendarmanager.getDayDetails(yearMonthDay[0], yearMonthDay[1], yearMonthDay[2]);
+	    
+	    DefaultTableModel dtm = new DefaultTableModel(data, header);
+	    day_Sales_Table.setModel(dtm);
+	    
+	    
+	    
+	    DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) day_Sales_Table.getDefaultRenderer(getClass());
+		renderer.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		
+		day_Sales_Table.setDefaultRenderer(day_Sales_Table.getColumnClass(0), renderer);
+	   
+		
+	    JScrollPane sp=new JScrollPane(day_Sales_Table);
+	    sp.setBorder(new LineBorder(Color.black, 2));
+	    
+	    sales_status.saleCalendarPanel.add(sp);	
+	    sales_status.saleCalendarPanel.setVisible(false);
+			
+		
+		
+		System.out.println("조회 완료");
+	}
+	
+	int test=-1;
+	public void CreateYearDetail(Sales_Status sales_status) { //해당 년도의 상세 페이지 생성
+		
+	
+		sales_status.detailPanel.removeAll();
+		sales_status.detailPanel.setLayout(new GridLayout(2,1,10,10));
+		
+		JTable year_Sales_Table=new JTable();
+		
+		Object[] header = {"영수증 번호","메뉴 카테고리","메뉴","금액","개수","메뉴별 합 금액","총 금액"};
+	    Object[][] data = calendarmanager.getYearDetails(yearMonthDay[0]);
+	 
+	    DefaultTableModel dtm = new DefaultTableModel(data, header);
+	    year_Sales_Table.setModel(dtm);
+	    
+	    DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) year_Sales_Table.getDefaultRenderer(getClass());
+		renderer.setHorizontalAlignment(SwingConstants.CENTER);
+
+		year_Sales_Table.setDefaultRenderer(year_Sales_Table.getColumnClass(0), renderer);
+	   
+		
+	    JScrollPane sp=new JScrollPane(year_Sales_Table);
+	    sp.setBorder(new LineBorder(Color.black, 2));
+	    
+	    int salecount=0,sum=0;
+	    for(int i=0; i<data.length; i++) {
+	    		if(data[i][0]!=null) {
+	    			salecount+=1;
+	    		}
+	    	
+	    		
+	    		if(data[i][6]!=null) {
+	    			String str=(String)data[i][6];
+	    			str=str.replaceAll(",", "");
+	    	    	
+		    		sum+=Integer.valueOf(str);
+	    		}
+	    		
+	    }
+	  
+	
+	    DecimalFormat addcomma=new DecimalFormat("###,###");
+
+	    sales_status.summary_Count_TextField.setText("판매건수 : "+salecount);
+	    sales_status.summary_sumPrice_TextField.setText("총 금액 : "+addcomma.format(sum)+"원");
+	
+	    
+	    sales_status.detailPanel.add(sp);	
+	    sales_status.detailPanel.add(sales_status.summaryPanel);	
+	 
+
+	}
+	
+public void CreateMonthDetail(Sales_Status sales_status) { //해당 월의 상세 페이지 생성
+		
+	
+		sales_status.detailPanel.removeAll();
+		sales_status.detailPanel.setLayout(new GridLayout(2,1,10,10));
+		
+		JTable month_Sales_Table=new JTable();
+		
+		Object[] header = {"영수증 번호","메뉴 카테고리","메뉴","금액","개수","메뉴별 합 금액","총 금액"};
+	    Object[][] data = calendarmanager.getMonthDetails(yearMonthDay[0],yearMonthDay[1]); 
+	 
+	    DefaultTableModel dtm = new DefaultTableModel(data, header);
+	    month_Sales_Table.setModel(dtm);
+	    
+	    DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) month_Sales_Table.getDefaultRenderer(getClass());
+		renderer.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		
+		month_Sales_Table.setDefaultRenderer(month_Sales_Table.getColumnClass(0), renderer);
+	   
+		
+	    JScrollPane sp=new JScrollPane(month_Sales_Table);
+	    sp.setBorder(new LineBorder(Color.black, 2));
+	    
+	    int count=0,sum=0;
+	    for(int i=0; i<data.length; i++) {
+	    		if(data[i][0]!=null) {
+	    			count+=1;
+	    		}
+	    		
+	    		if(data[i][6]!=null) {
+	    			String str=(String)data[i][6];
+	    			str=str.replaceAll(",", "");
+	    	    	
+		    		sum+=Integer.valueOf(str);
+	    		}
+	    		
+	    }
+	    
+	    DecimalFormat addcomma=new DecimalFormat("###,###");
+	    
+	    sales_status.summary_Count_TextField.setText("판매건수 : "+count);
+	    sales_status.summary_sumPrice_TextField.setText("총 금액 : "+addcomma.format(sum));
+	    
+	    sales_status.detailPanel.add(sp);	
+	    sales_status.detailPanel.add(sales_status.summaryPanel);	
+
+	   
+
+	}
+	
+	
+
+}
--- src/kukirun/gui/Sales_Status.java
+++ src/kukirun/gui/Sales_Status.java
@@ -38,43 +38,48 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
 import gui.util.CreateComponentUtil;
+import kukirun.manager.SalesCalendarManager;
 import ssz.gui.FrameTemplate3;
 
 
 public class Sales_Status extends FrameTemplate3{
-	DBconnection_SalesDetail connection=new DBconnection_SalesDetail();
+	//DBconnection_SalesDetail connection=new DBconnection_SalesDetail();
 	
 	
 	CreateComponentUtil ccUtil = new CreateComponentUtil();
-	JPanel mainPanel;
+	SalesCalendar createslaescalendar=new SalesCalendar();
+	SalesCalendarManager salesmanager=new SalesCalendarManager();
+	Sales_Status thisinstance=this;
+	
+	public JPanel mainPanel;
 
 	
-	JLabel titleLabel;
+	public JLabel titleLabel;
 	
-	JLabel yearLabel;
-	JPanel yearListPanel;
-	JComboBox<Integer> yearList;
+	public JLabel yearLabel;
+	public JPanel yearListPanel;
+	public JComboBox<Integer> yearList;
 	
-	JLabel monthLabel;
-	JPanel monthListPanel;	
-	JComboBox<Integer> monthList;
+	public JLabel monthLabel;
+	public JPanel monthListPanel;	
+	public JComboBox<Integer> monthList;
 	
-	JButton checkBtn;
+	public JButton checkBtn;
 	
-	JTabbedPane tab;
-	JPanel contentPanel;
-	JLabel contentNowLabel;
-	JPanel saleCalendarPanel;
-	JPanel detailPanel;
-	JTable sale_table;
-	ArrayList<JList> jlistList;
-	ArrayList<Integer> arr= new ArrayList<Integer>();
+	public JTabbedPane tab;
+	public JPanel contentPanel;
+	public JLabel contentNowLabel;
+	public JPanel saleCalendarPanel;
+	public JPanel detailPanel;
+	public JTable sale_table;
+	public ArrayList<JList> jlistList;
+	public ArrayList<Integer> arr= new ArrayList<Integer>();
 	
-	JPanel summaryPanel;
-	JPanel summary_sumPrice_Panel;
-	JTextField summary_sumPrice_TextField;
-	JPanel summary_Count_Panel;
-	JTextField summary_Count_TextField;
+	public JPanel summaryPanel;
+	public JPanel summary_sumPrice_Panel;
+	public JTextField summary_sumPrice_TextField;
+	public JPanel summary_Count_Panel;
+	public JTextField summary_Count_TextField;
 	
 	
 	
@@ -220,12 +225,12 @@ public class Sales_Status extends FrameTemplate3{
 				// TODO Auto-generated method stub
 				
 				if(monthList.getSelectedItem()!=null) { //month콤보박스가 null이 아니면 해당 월의 day가 출력되는 달력 만들고 해당 월의 상세 페이지 생성
-					CreateSaleCalender_Day((int) monthList.getSelectedItem()); 
-					CreateMonthDetail();
+					createslaescalendar.CreateSaleCalender_Days(thisinstance, (int) monthList.getSelectedItem()); 
+					createslaescalendar.CreateMonthDetail(thisinstance);
 				}
 				else { //month콤보박스가 null이면 년도의 month가 출력되는 달력 만들고 해당 년도의 상세 페이지 생성
-					CreateSaleCalender_Month();
-					CreateYearDetail();
+					createslaescalendar.CreateSaleCalender_Months(thisinstance);
+					createslaescalendar.CreateYearDetail(thisinstance);
 				}
 				
 			}
@@ -235,316 +240,6 @@ public class Sales_Status extends FrameTemplate3{
 		
 	}
 	
-	int createMonth=0;
-	int[] yearMonthDay= {0,0,0};
-	public void CreateSaleCalender_Month() { // 해당 년도의 1~12월 판넬 생성
-		jlistList.clear();
-		saleCalendarPanel.removeAll();
-		saleCalendarPanel.setLayout(new GridLayout(2,6,5,5));
-		
-		yearMonthDay[0]=(int) yearList.getSelectedItem();
-		contentNowLabel.setText(yearMonthDay[0]+"년");
-		
-		
-		if(yearList.getSelectedItem()==null) return;
-		
-		
-	
-		
-		jlistList = new ArrayList<JList>();
-		for(createMonth=0;createMonth<12;createMonth++) {
-			DecimalFormat addcomma=new DecimalFormat("###,###");
-			int month_sales=connection.getMonthSales((int)yearList.getSelectedItem(),(createMonth+1));
-			
-			
-			
-			
-			DefaultListModel<String> lmodel=new DefaultListModel<String>();
-			lmodel.addElement((createMonth+1)+"월");
-			lmodel.addElement("　");
-			lmodel.addElement("　");
-			lmodel.addElement("　");
-			lmodel.addElement("매출 : "+addcomma.format(month_sales));
-			
-			
-			JList<String> monthJlist=new JList<String>(lmodel);
-			
-			DefaultListCellRenderer renderer = (DefaultListCellRenderer) monthJlist.getCellRenderer();
-			renderer.setHorizontalAlignment(SwingConstants.CENTER); 
-			renderer.setVerticalAlignment(SwingConstants.CENTER);
-			monthJlist.setBorder(BorderFactory.createEmptyBorder(35, 0, 0, 0));
-			monthJlist.setFont(new Font("맑은 고딕",Font.BOLD, 20));
-		
-			jlistList.add(monthJlist);			
-			monthJlist.setBackground(new Color(0xC6D9F1));
-			
-			String name=String.valueOf(createMonth+1);
-			monthJlist.setName(name);
-			monthJlist.addMouseListener(new MouseAdapter() {
-
-				@Override
-				public void mouseClicked(MouseEvent e) {
-					// TODO Auto-generated method stub
-					contentNowLabel.setText(yearMonthDay[0]+"년"+name+"월");
-					CreateSaleCalender_Day(Integer.valueOf(name));
-					CreateMonthDetail();
-				}
-				
-			});
-			
-			
-			saleCalendarPanel.add(monthJlist);			
-			saleCalendarPanel.setVisible(false);
-			
-		}
-		createMonth=0;
-		System.out.println("조회 완료");
-	}
-	
-	int createDay=0;
-	public void CreateSaleCalender_Day(int month) { // 해당 월의 1~31일 달력 만듦
-		
-		jlistList.clear();
-		saleCalendarPanel.removeAll();
-		saleCalendarPanel.setLayout(new GridLayout(0,7,5,5));
-		yearMonthDay[0]=(int)yearList.getSelectedItem();
-
-		yearMonthDay[1]=month;
-		
-		if(monthList.getSelectedItem()!=null) contentNowLabel.setText(yearMonthDay[0]+"년"+yearMonthDay[1]+"월");
-		
-		if(yearList.getSelectedItem()==null) return;
-		
-		Calendar carlendar=Calendar.getInstance();
-		carlendar.set(yearMonthDay[0], yearMonthDay[1]-1,1);
-		System.out.println(yearMonthDay[0]+","+yearMonthDay[1]+","+yearMonthDay[2]);
-		
-		int startday=carlendar.get(Calendar.DAY_OF_WEEK);
-		int lastday=carlendar.getActualMaximum(Calendar.DATE);
-		System.out.println(startday+","+lastday);
-		
-		String day = null;
-		for(int i=0; i<7;i++) {
-			switch(i) {
-			
-				case 0:day="일"; break;
-				case 1:day="월"; break;
-				case 2:day="화"; break;
-				case 3:day="수"; break;
-				case 4:day="목"; break;
-				case 5:day="금"; break;
-				case 6:day="토"; break;
-								
-			}
-			JLabel tempLabel=new JLabel(day);
-			tempLabel.setHorizontalAlignment(SwingConstants.CENTER);
-			tempLabel.setForeground(Color.white);
-			tempLabel.setFont(new Font("맑은 고딕",Font.BOLD, 20));
-			
-			
-			
-			JPanel tempPanel=new JPanel(new GridLayout());
-			tempPanel.setBackground(new Color(0x4F81BD));
-			tempPanel.add(tempLabel);
-			saleCalendarPanel.add(tempPanel);
-			
-
-		}
-
-	
-		for(int i=0; i<startday-1;i++) {
-
-			saleCalendarPanel.add(new JPanel());
-
-		}
-		
-		
-		jlistList = new ArrayList<JList>();
-		
-		
-		for(int createDay=1;createDay<lastday+1;createDay++) {
-			int day_sales=connection.getDaySales((int)yearList.getSelectedItem(),month,(createDay));
-			DecimalFormat addcomma=new DecimalFormat("###,###");
-			
-			DefaultListModel<String> lmodel=new DefaultListModel<String>();
-			lmodel.addElement((createDay)+"일");
-			lmodel.addElement("　");
-			lmodel.addElement("매출 : "+addcomma.format(day_sales));
-			
-			
-			JList<String> dayJlist=new JList<String>(lmodel);
-			
-			DefaultListCellRenderer renderer = (DefaultListCellRenderer) dayJlist.getCellRenderer();
-			renderer.setHorizontalAlignment(SwingConstants.CENTER); 
-			renderer.setVerticalAlignment(SwingConstants.CENTER);
-			dayJlist.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
-			dayJlist.setFont(new Font("맑은 고딕",Font.BOLD, 12));
-			
-			dayJlist.setName(String.valueOf(createDay));
-		
-			jlistList.add(dayJlist);			
-			jlistList.get(createDay-1).setBackground(new Color(0xC6D9F1));
-
-		
-			dayJlist.addMouseListener(new MouseAdapter() {
-
-				@Override
-				public void mouseClicked(MouseEvent e) {
-					// TODO Auto-generated method stub
-					CreateDayDetail(Integer.valueOf(dayJlist.getName()));
-				}
-				
-			});
-			
-			
-			saleCalendarPanel.add(dayJlist);			
-			saleCalendarPanel.setVisible(false);
-			
-		}
-		createDay=0;
-		System.out.println("조회 완료");
-	}
-	
-	
-	public void CreateDayDetail(int day) { //달력에서 Day 판넬을 클릭 했을 때 Day의 상세 페이지 생성
-		
-		jlistList.clear();
-		saleCalendarPanel.removeAll();
-		saleCalendarPanel.setLayout(new GridLayout(1,1,10,10));
-		
-		yearMonthDay[2]=day;
-		
-		JTable day_Sales_Table=new JTable();
-		
-		Object[] header = {"영수증 번호","메뉴 카테고리","메뉴","금액","개수","메뉴별 합 금액","총 금액"};
-	    Object[][] data = connection.getDayDetails(yearMonthDay[0], yearMonthDay[1], yearMonthDay[2]);
-	    
-	    DefaultTableModel dtm = new DefaultTableModel(data, header);
-	    day_Sales_Table.setModel(dtm);
-	    
-	    
-	    
-	    DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) day_Sales_Table.getDefaultRenderer(getClass());
-		renderer.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		
-		day_Sales_Table.setDefaultRenderer(day_Sales_Table.getColumnClass(0), renderer);
-	   
-		
-	    JScrollPane sp=new JScrollPane(day_Sales_Table);
-	    sp.setBorder(new LineBorder(Color.black, 2));
-	    
-		saleCalendarPanel.add(sp);	
-		saleCalendarPanel.setVisible(false);
-			
-		
-		
-		System.out.println("조회 완료");
-	}
-	int test=-1;
-	public void CreateYearDetail() { //해당 년도의 상세 페이지 생성
-		
-	
-		detailPanel.removeAll();
-		detailPanel.setLayout(new GridLayout(2,1,10,10));
-		
-		JTable year_Sales_Table=new JTable();
-		
-		Object[] header = {"영수증 번호","메뉴 카테고리","메뉴","금액","개수","메뉴별 합 금액","총 금액"};
-	    Object[][] data = connection.getYearDetails(yearMonthDay[0]);
-	 
-	    DefaultTableModel dtm = new DefaultTableModel(data, header);
-	    year_Sales_Table.setModel(dtm);
-	    
-	    DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) year_Sales_Table.getDefaultRenderer(getClass());
-		renderer.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		
-		year_Sales_Table.setDefaultRenderer(year_Sales_Table.getColumnClass(0), renderer);
-	   
-		
-	    JScrollPane sp=new JScrollPane(year_Sales_Table);
-	    sp.setBorder(new LineBorder(Color.black, 2));
-	    
-	    int salecount=0,sum=0;
-	    for(int i=0; i<data.length; i++) {
-	    		if(data[i][0]!=null) {
-	    			salecount+=1;
-	    		}
-	    	
-	    		
-	    		if(data[i][6]!=null) {
-	    			String str=(String)data[i][6];
-	    			str=str.replaceAll(",", "");
-	    	    	
-		    		sum+=Integer.valueOf(str);
-	    		}
-	    		
-	    }
-	  
-	
-	    DecimalFormat addcomma=new DecimalFormat("###,###");
-
-	    summary_Count_TextField.setText("판매건수 : "+salecount);
-	    summary_sumPrice_TextField.setText("총 금액 : "+addcomma.format(sum)+"원");
-	
-	    
-	    detailPanel.add(sp);	
-	    detailPanel.add(summaryPanel);	
-	 
-
-	}
-	
-public void CreateMonthDetail() { //해당 월의 상세 페이지 생성
-		
-	
-		detailPanel.removeAll();
-		detailPanel.setLayout(new GridLayout(2,1,10,10));
-		
-		JTable month_Sales_Table=new JTable();
-		
-		Object[] header = {"영수증 번호","메뉴 카테고리","메뉴","금액","개수","메뉴별 합 금액","총 금액"};
-	    Object[][] data = connection.getMonthDetails(yearMonthDay[0],yearMonthDay[1]);
-	 
-	    DefaultTableModel dtm = new DefaultTableModel(data, header);
-	    month_Sales_Table.setModel(dtm);
-	    
-	    DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) month_Sales_Table.getDefaultRenderer(getClass());
-		renderer.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		
-		month_Sales_Table.setDefaultRenderer(month_Sales_Table.getColumnClass(0), renderer);
-	   
-		
-	    JScrollPane sp=new JScrollPane(month_Sales_Table);
-	    sp.setBorder(new LineBorder(Color.black, 2));
-	    
-	    int count=0,sum=0;
-	    for(int i=0; i<data.length; i++) {
-	    		if(data[i][0]!=null) {
-	    			count+=1;
-	    		}
-	    		
-	    		if(data[i][6]!=null) {
-	    			String str=(String)data[i][6];
-	    			str=str.replaceAll(",", "");
-	    	    	
-		    		sum+=Integer.valueOf(str);
-	    		}
-	    		
-	    }
-	    
-	    DecimalFormat addcomma=new DecimalFormat("###,###");
-	    
-	    summary_Count_TextField.setText("판매건수 : "+count);
-	    summary_sumPrice_TextField.setText("총 금액 : "+addcomma.format(sum));
-	    
-	    detailPanel.add(sp);	
-	    detailPanel.add(summaryPanel);	
-
-	   
-
-	}
 	
 
 	
