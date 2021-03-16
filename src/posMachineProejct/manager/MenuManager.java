6ba71d7153c0daadb27529d4187ea2da046fac78
Webservice <67294348+lsx2003@users.noreply.github.com>
2021-03-16 20:53:35 +0900

    Update MenuManager.java
    CRUD 구현

--- src/posMachineProejct/manager/MenuManager.java
+++ src/posMachineProejct/manager/MenuManager.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import data.CategoryVO;
 import data.MenuVO;
+import data.OrderDetailVO;
 import posMachineProject.service.MenuService;
 import posMachineProject.serviceImpl.MenuServiceImpl;
 
@@ -18,11 +19,28 @@ public class MenuManager {
 		return ms.menuListByCategoryName(categoryName);
 	}
 
-	
 	public List<CategoryVO> selectCategoryList(){
 		return ms.categoryList();
 
 	}
+
+	public int insertItemDetail(int categoryNum, String menuName, int menuPrice, int sequence) {
+		return ms.insertItemDetail(categoryNum, menuName, menuPrice, sequence);
+	}
+	
+
+	public int insertCategoryDetail(int num, String categoryName) {
+		return ms.insertCategoryDetail(num, categoryName);
+	}
+	
+	public int deleteItemDetail(String menuName) {
+		return ms.deleteItemDetail(menuName);
+	}
+	
+	public int deleteCategoryDetail(String categoryName) {
+		return ms.deleteCategoryDetail(categoryName);
+	}
+
 
 }
 
