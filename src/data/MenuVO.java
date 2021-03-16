c84aed550323a4e97868ff49cdeccb28a516471e
Webservice <67294348+lsx2003@users.noreply.github.com>
2021-03-16 20:57:09 +0900

    Update MenuVO.java
    MenuVO 수정

--- src/data/MenuVO.java
+++ src/data/MenuVO.java
@@ -4,16 +4,26 @@ import java.sql.Date;
 
 public class MenuVO {
 	
+	
 	private String menuCategory;
 	private String menuName;
+	private int Category_num;
 	private int menuPrice;
+	private int Sequence;
 	
-
+	
+	
 	public String getMenuCategory() {
 		return menuCategory;
 	}
 	public void setMenuCategory(String menuCategory) {
 		this.menuCategory = menuCategory;
+	}
+	public int getCategory_num() {
+		return Category_num;
+	}
+	public void setCategory_num(int category_num) {
+		this.Category_num = category_num;
 	}
 	public String getMenuName() {
 		return menuName;
@@ -27,12 +37,17 @@ public class MenuVO {
 	public void setMenuPrice(int menuPrice) {
 		this.menuPrice = menuPrice;
 	}
-
-	
+	public int getSequence() {
+		return Sequence;
+	}
+	public void setSequence(int sequence) {
+		Sequence = sequence;
+	}
 	@Override
 	public String toString() {
-		return "MenuVO [menuCategory=" + menuCategory + ", menuName=" + menuName + ", menuPrice=" + menuPrice + "]";
+		return "MenuVO [menuCategory=" + menuCategory + ", menuName=" + menuName + ", Category_num=" + Category_num
+				+ ", menuPrice=" + menuPrice + ", Sequence=" + Sequence + "]";
 	}
 
-		
+	
 }
