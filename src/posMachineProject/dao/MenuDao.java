a059cb37fa21f205052811996dbe6fabedc87f69
Webservice <67294348+lsx2003@users.noreply.github.com>
2021-03-16 20:49:07 +0900

    Update MenuDao.java
    CRUD 업데이트

--- src/posMachineProject/dao/MenuDao.java
+++ src/posMachineProject/dao/MenuDao.java
@@ -4,10 +4,16 @@ import java.util.List;
 
 import data.CategoryVO;
 import data.MenuVO;
+import data.OrderDetailVO;
 
 public interface MenuDao {
-	public abstract List<MenuVO> menuList() throws Exception;
-	
+	public abstract List<MenuVO> menuList() throws Exception;    
 	public abstract List<CategoryVO> categoryList() throws Exception;
 	public abstract List<MenuVO> menuListByCategoryName(String categoryName) throws Exception;
+	public abstract void insertItemDetail(MenuVO vo) throws Exception;
+	public abstract void insertCategoryDetail(CategoryVO vo) throws Exception;
+	public abstract void deleteItemDetail(MenuVO vo) throws Exception;
+	public abstract void deleteCategoryDetail(CategoryVO vo) throws Exception;
+
+
 }
