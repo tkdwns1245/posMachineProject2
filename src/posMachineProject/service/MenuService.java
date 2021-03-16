a2c10dfa8b8f1ce51ec14a403c0ec4ef8481b133
Webservice <67294348+lsx2003@users.noreply.github.com>
2021-03-16 20:50:50 +0900

    Update MenuService.java
    CRUD 업데이트

--- src/posMachineProject/service/MenuService.java
+++ src/posMachineProject/service/MenuService.java
@@ -4,9 +4,19 @@ import java.util.List;
 
 import data.CategoryVO;
 import data.MenuVO;
+import data.OrderDetailVO;
 
 public interface MenuService {
 	public abstract List<MenuVO> menuList(); 
 	public abstract List<CategoryVO> categoryList(); 
 	public abstract List<MenuVO> menuListByCategoryName(String categoryName);
+	public abstract void saveItemDetailList(List<MenuVO> insertList, List<MenuVO> deleteList);
+	public abstract int insertItemDetail(int categoryNum, String menuName, int menuPrice, int sequence);
+	public abstract int insertCategoryDetail(int num, String CategoryName);
+	public abstract int deleteItemDetail(String menuName);
+	public abstract int deleteCategoryDetail(String categoryName);
+
+
+
+
 }
