954543c549c79fda7fe72744adf7b99725650461
Webservice <67294348+lsx2003@users.noreply.github.com>
2021-03-16 20:52:46 +0900

    Update MenuServiceImpl.java
    CRUD 구현

--- src/posMachineProject/serviceImpl/MenuServiceImpl.java
+++ src/posMachineProject/serviceImpl/MenuServiceImpl.java
@@ -1,9 +1,12 @@
 package posMachineProject.serviceImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import data.CategoryVO;
 import data.MenuVO;
+import data.OrderDetailVO;
+import data.ReceiptJoinedVO;
 import posMachineProject.daoImpl.MenuDaoImpl;
 import posMachineProject.service.MenuService;
 
@@ -42,5 +45,74 @@ public class MenuServiceImpl implements MenuService{
 		return null;
 	}
 	
+
+	@Override
+	public int insertItemDetail(int categoryNum, String menuName, int menuPrice, int sequence) {
+		MenuVO menuVO = new MenuVO();
+		try {
+			menuVO.setCategory_num(categoryNum);
+			menuVO.setMenuName(menuName);
+			menuVO.setMenuPrice(menuPrice);
+			menuVO.setSequence(sequence);
+			menuVO.setCategory_num(categoryNum);
+			MenuDao.insertItemDetail(menuVO);
+			
+			return 1;
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 2;		
+	}
+
+	@Override
+	public int insertCategoryDetail(int num, String categoryName) {
+		CategoryVO categoryVO = new CategoryVO();
+		try {
+			categoryVO.setNum(num);
+			categoryVO.setCategoryName(categoryName);
+			MenuDao.insertCategoryDetail(categoryVO);
+			return 1;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 2;
+	}
+
+	@Override
+	public int deleteItemDetail(String menuName) {
+		MenuVO menuVO = new MenuVO();
+		try {
+			menuVO.setMenuName(menuName);
+			MenuDao.deleteItemDetail(menuVO);
+			
+			return 1;
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 2;		
+	}
+	
+	@Override
+	public int deleteCategoryDetail(String categoryName) {
+		CategoryVO categoryVO = new CategoryVO();
+		try {
+			categoryVO.setCategoryName(categoryName);
+			MenuDao.deleteCategoryDetail(categoryVO);
+			
+			return 1;
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 2;		
+	}
+
+
 
 }
